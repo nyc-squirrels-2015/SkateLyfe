0e97c70caa0def06d3835b3a1ee29c0ffadfc0a8
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2015-02-18 11:46:39 -0500

    Add ajax -to success- for login

--- public/js/application.js
+++ public/js/application.js
@@ -12,6 +12,24 @@ $(document).ready(function() {
  			$(".signup_form").toggle(); 			
  	});
 
+ 	$(".login_ajax").on("submit", function(event){
+		event.preventDefault();
+		var $form = $(event.target)
+
+		$.ajax({
+			url:  $form.attr('action'),
+			type: $form.attr('method'),
+			data: $form.serialize(),
+			success: function(response){
+				$(".auth_btns").remove();
+				$("#content").prepend(response);
+				console.log(response);
+			},
+			error: function(){
+
+			} 
+		});
+	});
 
 
 });
