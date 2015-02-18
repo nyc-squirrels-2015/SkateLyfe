e8627524f59e0e4106ef1b8b96f908f43af7efd6
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2015-02-18 11:21:13 -0500

    Access login and signup btns

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+ 	
+ 	$(".login_btn").on("click", function(event){
+ 			event.preventDefault();
+ 			console.log("YOU CLICKED")
+ 	});
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+ 	$(".signup_btn").on("click", function(event){
+ 			event.preventDefault();
+ 			console.log("YOU CLICKED")
+ 	});
+
+
+
 });
