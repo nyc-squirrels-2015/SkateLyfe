af768db8872843eac8d959f19758498ae02fb3c7
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2015-02-18 10:39:42 -0500

    Add signup and logout routes

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,20 +1,41 @@
+
+################### LOGIN ##############################
 get '/login' do
-	erb :login
+	erb :'auth/login'
 end
 
 post '/login' do
 	@user = User.find_by( username: params[:user][:username] )
-
 	if @user.try(:authenticate, params[:user][:password])
-		session[:id] = @user.id
-		
+		session[:id] = @user.id		
 		# this will be for when i create the ajax call later
 		if request.xhr?
 			erb :'_partials/_auth', layout: false
 		end
-	
 	else
 		return 401
-	end	
-
+	end
 end
+
+################### SIGNUP ##############################
+get '/signup' do
+	erb :'auth/signup'
+end
+
+post '/signup' do
+	@user = User.new( params[:user] )
+	if @user.save
+		session[:id] = user.id
+		redirect '/'
+	else
+		return 401	
+	end
+end
+
+
+################### LOGOUT ##############################
+get '/logout' do
+	session[:id] = nil
+	redirect '/'
+end
+
