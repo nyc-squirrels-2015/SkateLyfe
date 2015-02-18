62c9adefd4fe04079e8ead518e7b2cd01d8974d0
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2015-02-18 11:26:02 -0500

    Add toggle functionality to auth btns

--- public/js/application.js
+++ public/js/application.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
  	
  	$(".login_btn").on("click", function(event){
  			event.preventDefault();
- 			console.log("YOU CLICKED")
+ 			console.log("YOU CLICKED");
+ 			$(".login_form").toggle();
  	});
 
  	$(".signup_btn").on("click", function(event){
  			event.preventDefault();
- 			console.log("YOU CLICKED")
+ 			console.log("YOU CLICKED");
+ 			$(".signup_form").toggle(); 			
  	});
 
 
